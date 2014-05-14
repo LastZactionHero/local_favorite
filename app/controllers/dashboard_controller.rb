b60f8b19707117d7f6bd83fceec4badcc08fedc1
Zach Dicklin <zdicklin@gmail.com>
2014-05-13 19:03:38 -0600

    Ordering changes

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,8 +3,12 @@ class DashboardController < ApplicationController
 
   def index
     @search_terms = current_user.search_terms
-    @favorites = current_user.favorites.where("created_at > ?", 1.day.ago)
-    @tweets = current_user.tweets.where("created_at > ?", 1.day.ago)
+    
+    @favorites = current_user.favorites.where("created_at > ?", 1.day.ago).
+      order("created_at DESC")
+
+    @tweets = current_user.tweets.where("created_at > ?", 1.day.ago).
+      order("created_at DESC")
   end
 
 end
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,7 +2,7 @@ class TweetsController < ApplicationController
 
   def index
     @search_terms = current_user.search_terms
-    @tweets = current_user.tweets.order("id DESC").limit(100)
+    @tweets = current_user.tweets.order("created_at DESC").limit(100)
   end
 
 end
