ab93e7c7bcc513dd5e6faa848e08c572e7b62314
Zach Dicklin <zdicklin@gmail.com>
2014-11-04 19:12:30 -0700

    Better error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,18 +3,18 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  rescue_from CanCan::AccessDenied do |exception|
-    if current_user
-      redirect_to dashboard_path, alert: exception.message
-    else
-      redirect_to user_omniauth_authorize_path(:twitter)
-    end
-  end
-
-  if Rails.env.production?
-    rescue_from Exception do |exception|
+  rescue_from Exception do |exception|
+    if exception.is_a?(CanCan::AccessDenied)
+      if current_user
+        redirect_to dashboard_path, alert: exception.message
+      else
+        redirect_to user_omniauth_authorize_path(:twitter)
+      end
+    elsif Rails.env.production?
       ExceptionMailer.exception_email(exception, current_user).deliver
       render template: false, file: 'public/500.html', status: 500
+    else
+      raise exception
     end
   end
 
